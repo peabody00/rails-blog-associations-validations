30fa51012fa3cb95b647b5bea437aeda333d5678
Charlie <peabody00@gmail.com>
2021-04-12 09:50:45 -0500

    commit tree aa2865e8d5941ab0d5246a6969e0f559c5646716

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
+    belongs_to :user
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+
+    validates :name, presence: true
+    validates :content, presence: true
+
+    # accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes[:name].blank? }
   
 end
